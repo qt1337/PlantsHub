67656a4989a9352a4276b34c52a8b5ef27b3ba9c
liza-kl <l.kletsko0202@gmail.com>
2020-10-01 15:50:31 +0200

    server.js gefixed

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/plants-hub'));
+app.use(express.static('./dist/PlantsHub'));
 
-app.get('*', (req, res) =>
-  res.sendFile('dist/plants-hub/index.html'),
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/PlantsHub/'}),
 );
 
 app.listen(process.env.PORT || 8080);
