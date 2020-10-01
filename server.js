ef6669514f7ade5b160e06110b34ff757a38f28d
liza-kl <l.kletsko0202@gmail.com>
2020-10-01 15:46:56 +0200

    server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express();
 
 app.use(express.static('./dist/plants-hub'));
 
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/plants-hub/'}),
+app.get('*', (req, res) =>
+  res.sendFile('dist/plants-hub/index.html'),
 );
 
 app.listen(process.env.PORT || 8080);
