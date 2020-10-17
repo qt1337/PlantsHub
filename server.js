b3521001f62e077bed6dd65db22883bf9dcd305d
Restyled.io <commits@restyled.io>
2020-10-17 16:29:30 +0000

    Restyled by clang-format

--- server.js
+++ server.js
@@ -3,22 +3,21 @@ require('dotenv').config();
 const express = require('express');
 const mariadb = require('mariadb');
 
-const pool = mariadb.createPool(
-  {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    connectionLimit: 5
-  }
-);
+const pool = mariadb.createPool({
+  host : process.env.DB_HOST,
+  user : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_DATABASE,
+  connectionLimit : 5
+});
 
 const app = express();
 
 app.use(express.static('./dist/PlantsHub'));
 
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/PlantsHub/'}),
+app.get(
+    '/*',
+    (req, res) => res.sendFile('index.html', {root : 'dist/PlantsHub/'}),
 );
 
 app.listen(process.env.PORT || 8080, () => {
@@ -27,24 +26,27 @@ app.listen(process.env.PORT || 8080, () => {
 
 app.post('/api/create-item/:item_id/:item_name', (req, res) => {
   pool.getConnection()
-    .then(conn => {
+      .then(conn => {
 
-      conn.query("SELECT 1 as val")
-        .then((rows) => {
-          console.log(rows);
-          return conn.query("INSERT INTO myTable value (?, ?)", [req.params.item_id, req.params.item_name]);
-        })
-        .then((resp) => {
-          res.status(200).send("rows have been created");
-          console.log(resp);
-          conn.end();
-        })
-        .catch(err => {
-          res.status(200).send("rows could not be created");
-          conn.end();
-        })
+                conn.query("SELECT 1 as val")
+                    .then((rows) => {
+                      console.log(rows);
+                      return conn.query(
+                          "INSERT INTO myTable value (?, ?)",
+                          [ req.params.item_id, req.params.item_name ]);
+                    })
+                    .then((resp) => {
+                      res.status(200).send("rows have been created");
+                      console.log(resp);
+                      conn.end();
+                    })
+                    .catch(err => {
+                      res.status(200).send("rows could not be created");
+                      conn.end();
+                    })
 
-    }).catch(err => {
-    //not connected
-  });
+            })
+      .catch(err => {
+                 // not connected
+             });
 });
