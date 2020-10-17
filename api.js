ebd55d9372e6d433c7ad16263fd705139dbb6229
Restyled.io <commits@restyled.io>
2020-10-17 18:27:04 +0000

    Restyled by clang-format

--- api.js
+++ api.js
@@ -1,29 +1,27 @@
 function createItem(pool, req, res) {
-  pool
-    .getConnection()
-    .then((conn) => {
-      conn
-        .query("SELECT 1 as val")
-        .then((rows) => {
-          console.log(rows);
-          return conn.query("INSERT INTO myTable value (?, ?)", [
-            req.params.item_id,
-            req.params.item_name,
-          ]);
-        })
-        .then((resp) => {
-          res.status(200).send("rows have been created");
-          console.log(resp);
-          conn.end();
-        })
-        .catch((err) => {
-          res.status(200).send("rows could not be created");
-          conn.end();
-        });
-    })
-    .catch((err) => {
-      // not connected
-    });
+  pool.getConnection()
+      .then((conn) => {
+        conn.query("SELECT 1 as val")
+            .then((rows) => {
+              console.log(rows);
+              return conn.query("INSERT INTO myTable value (?, ?)", [
+                req.params.item_id,
+                req.params.item_name,
+              ]);
+            })
+            .then((resp) => {
+              res.status(200).send("rows have been created");
+              console.log(resp);
+              conn.end();
+            })
+            .catch((err) => {
+              res.status(200).send("rows could not be created");
+              conn.end();
+            });
+      })
+      .catch((err) => {
+                 // not connected
+             });
 }
 
-module.exports = { createItem }
+module.exports = {createItem}
