664a4e45dc77d10cfbeedf3b3529027c9b3a3b94
liza-kl <l.kletsko0202@gmail.com>
2020-10-20 10:34:22 +0200

    :construction: started to integrate cordova

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,9 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+/* For Cordova */
+let onDeviceReady = () => {
+  platformBrowserDynamic().bootstrapModule(AppModule);
+};
+document.addEventListener('deviceready', onDeviceReady, false);
