45d38830c09793970299192ca69eb2af50ef98d0
Restyled.io <commits@restyled.io>
2020-10-23 12:14:40 +0000

    Restyled by clang-format

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const mariadb = require("mariadb");
 const cookieParser = require("cookie-parser");
 
 const pool = mariadb.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  connectionLimit: 5,
+  host : process.env.DB_HOST,
+  user : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_DATABASE,
+  connectionLimit : 5,
 });
 
 const app = express();
@@ -18,26 +18,20 @@ const app = express();
 app.use(express.static("./dist/PlantsHub"));
 app.use(cookieParser());
 
-app.get("/*", (req, res) =>
-  res.sendFile("index.html", {root: "dist/PlantsHub/"})
-);
+app.get("/*",
+        (req, res) => res.sendFile("index.html", {root : "dist/PlantsHub/"}));
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Server is listening on port ${process.env.PORT || 8080}`);
 });
 
-app.post("/api/create-item/:item_id/:item_name", (req, res) => {
-  api.createItem(pool, req, res);
-});
+app.post("/api/create-item/:item_id/:item_name",
+         (req, res) => { api.createItem(pool, req, res); });
 
-app.post("/api/create-user", (req, res) => {
-  api.createUser(pool, req, res);
-});
+app.post("/api/create-user", (req, res) => { api.createUser(pool, req, res); });
 
-app.post("/api/check-credentials", (req, res) => {
-  api.checkUserCredentials(pool, req, res);
-});
+app.post("/api/check-credentials",
+         (req, res) => { api.checkUserCredentials(pool, req, res); });
 
-app.post("/api/check-session", (req, res) => {
-  api.checkUserSession(pool, req, res);
-});
+app.post("/api/check-session",
+         (req, res) => { api.checkUserSession(pool, req, res); });
