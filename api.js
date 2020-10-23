aaa4d80ee953c46e5f765eb2f4bb1bb5b14a3d24
Restyled.io <commits@restyled.io>
2020-10-23 11:13:05 +0000

    Restyled by prettier

--- api.js
+++ api.js
@@ -6,9 +6,10 @@ const crypto = require("crypto");
  * @param {number} length - Length of the random string.
  */
 function getRandomString(length) {
-  return crypto.randomBytes(Math.ceil(length / 2))
-      .toString("hex")
-      .slice(0, length);
+  return crypto
+    .randomBytes(Math.ceil(length / 2))
+    .toString("hex")
+    .slice(0, length);
 }
 
 /**
@@ -22,7 +23,7 @@ function sha512(password, salt) {
   /** Hashing algorithm sha512 */
   hash.update(password);
   let value = hash.digest("hex");
-  return {salt : salt, passwordHash : value};
+  return { salt: salt, passwordHash: value };
 }
 
 /**
@@ -33,38 +34,40 @@ function sha512(password, salt) {
 function getSaltHashPassword(password) {
   let salt = getRandomString(16);
   let passwordData = sha512(password, salt);
-  return [ salt, passwordData.passwordHash ];
+  return [salt, passwordData.passwordHash];
 }
 
 /**
  * Creates a dummy item
  */
 function createItem(pool, req, res) {
-  pool.getConnection()
-      .then((conn) => {
-        conn.query("SELECT 1 as val")
-            .then((rows) => {
-              console.log(rows);
-              return conn.query("INSERT INTO myTable value (?, ?)", [
-                req.params.item_id,
-                req.params.item_name,
-              ]);
-            })
-            .then((result) => {
-              res.status(200).send("rows have been created");
-              console.log(result);
-              conn.end();
-            })
-            .catch((err) => {
-              console.log(err);
-              res.status(200).send("rows could not be created");
-              conn.end();
-            });
-      })
-      .catch((err) => {
-        console.log(err);
-        // not connected
-      });
+  pool
+    .getConnection()
+    .then((conn) => {
+      conn
+        .query("SELECT 1 as val")
+        .then((rows) => {
+          console.log(rows);
+          return conn.query("INSERT INTO myTable value (?, ?)", [
+            req.params.item_id,
+            req.params.item_name,
+          ]);
+        })
+        .then((result) => {
+          res.status(200).send("rows have been created");
+          console.log(result);
+          conn.end();
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(200).send("rows could not be created");
+          conn.end();
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      // not connected
+    });
 }
 
 /**
@@ -73,31 +76,37 @@ function createItem(pool, req, res) {
 function createUser(pool, req, res) {
   let [salt, hashedPassword] = getSaltHashPassword(req.query.user_password);
 
-  pool.getConnection()
-      .then((connection) => {
-        connection
-            .query(
-                "INSERT INTO User(username, email, password, forename, surname, birthday, salt) value (?, ?, ?, ?, ?, ?, ?)",
-                [
-                  req.query.user_username, req.query.user_email, hashedPassword,
-                  req.query.user_forename, req.query.user_surname,
-                  req.query.user_birthday, salt
-                ])
-            .then((result) => {
-              res.status(202).send("rows have been created");
-              console.log(result);
-              connection.end();
-            })
-            .catch((err) => {
-              console.log(err);
-              res.status(200).send("rows could not be created");
-              connection.end();
-            });
-      })
-      .catch((err) => {
-        console.log(err);
-        // not connected
-      });
+  pool
+    .getConnection()
+    .then((connection) => {
+      connection
+        .query(
+          "INSERT INTO User(username, email, password, forename, surname, birthday, salt) value (?, ?, ?, ?, ?, ?, ?)",
+          [
+            req.query.user_username,
+            req.query.user_email,
+            hashedPassword,
+            req.query.user_forename,
+            req.query.user_surname,
+            req.query.user_birthday,
+            salt,
+          ]
+        )
+        .then((result) => {
+          res.status(202).send("rows have been created");
+          console.log(result);
+          connection.end();
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(200).send("rows could not be created");
+          connection.end();
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      // not connected
+    });
 }
 
 /**
@@ -108,34 +117,39 @@ function checkUserSession(pool, req, res) {
   let sessionId = req.query.sessionId;
   let hashedSessionId;
 
-  pool.getConnection()
-      .then((conn) => {
-        conn.query("SELECT salt FROM User WHERE username = (?) OR email = (?)",
-                   [ account, account ])
-            .then((row) => {
-              let salt = row[0].salt;
+  pool
+    .getConnection()
+    .then((conn) => {
+      conn
+        .query("SELECT salt FROM User WHERE username = (?) OR email = (?)", [
+          account,
+          account,
+        ])
+        .then((row) => {
+          let salt = row[0].salt;
 
-              hashedSessionId = sha512(sessionId, salt).passwordHash;
+          hashedSessionId = sha512(sessionId, salt).passwordHash;
 
-              return conn.query(
-                  "SELECT * FROM User WHERE ( username = (?) OR email = (?) ) and sessionId = (?)",
-                  [ account, account, hashedSessionId ]);
-            })
-            .then((result) => {
-              console.log(result);
-              res.status(202).send("sessionId correct");
-              conn.end();
-            })
-            .catch((err) => {
-              console.log(err);
-              res.status(200).send("sessionId not correct");
-              conn.end();
-            });
-      })
-      .catch((err) => {
-        console.log(err);
-        // not connected
-      });
+          return conn.query(
+            "SELECT * FROM User WHERE ( username = (?) OR email = (?) ) and sessionId = (?)",
+            [account, account, hashedSessionId]
+          );
+        })
+        .then((result) => {
+          console.log(result);
+          res.status(202).send("sessionId correct");
+          conn.end();
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(200).send("sessionId not correct");
+          conn.end();
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      // not connected
+    });
 }
 
 /**
@@ -148,43 +162,48 @@ function checkUserCredentials(pool, req, res) {
   let hashedPassword;
   let hashedSessionId;
 
-  pool.getConnection()
-      .then((conn) => {
-        conn.query("SELECT salt FROM User WHERE username = (?) OR email = (?)",
-                   [ account, account ])
-            .then((row) => {
-              let salt = row[0].salt;
+  pool
+    .getConnection()
+    .then((conn) => {
+      conn
+        .query("SELECT salt FROM User WHERE username = (?) OR email = (?)", [
+          account,
+          account,
+        ])
+        .then((row) => {
+          let salt = row[0].salt;
 
-              hashedPassword = sha512(password, salt).passwordHash;
-              hashedSessionId = sha512(sessionId, salt).passwordHash;
+          hashedPassword = sha512(password, salt).passwordHash;
+          hashedSessionId = sha512(sessionId, salt).passwordHash;
 
-              return conn.query(
-                  "UPDATE User SET sessionId = (?) WHERE ( username = (?) OR email = (?) ) and password = (?)",
-                  [ hashedSessionId, account, account, hashedPassword ]);
-            })
-            .then((result) => {
-              console.log(result);
-              res.status(202).send({
-                'sessionId' : sessionId,
-                'account' : account
-              }); // save to cookie
-              conn.end();
-            })
-            .catch((err) => {
-              console.log(err);
-              res.status(200).send("credentials not correct");
-              conn.end();
-            });
-      })
-      .catch((err) => {
-        console.log(err);
-        // not connected
-      });
+          return conn.query(
+            "UPDATE User SET sessionId = (?) WHERE ( username = (?) OR email = (?) ) and password = (?)",
+            [hashedSessionId, account, account, hashedPassword]
+          );
+        })
+        .then((result) => {
+          console.log(result);
+          res.status(202).send({
+            sessionId: sessionId,
+            account: account,
+          }); // save to cookie
+          conn.end();
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(200).send("credentials not correct");
+          conn.end();
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      // not connected
+    });
 }
 
 module.exports = {
   createItem,
   createUser,
   checkUserCredentials,
-  checkUserSession
+  checkUserSession,
 };
--- server.js
+++ server.js
@@ -5,31 +5,37 @@ const express = require("express");
 const mariadb = require("mariadb");
 
 const pool = mariadb.createPool({
-  host : process.env.DB_HOST,
-  user : process.env.DB_USER,
-  password : process.env.DB_PASSWORD,
-  database : process.env.DB_DATABASE,
-  connectionLimit : 5,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  connectionLimit: 5,
 });
 
 const app = express();
 
 app.use(express.static("./dist/PlantsHub"));
 
-app.get("/*",
-        (req, res) => res.sendFile("index.html", {root : "dist/PlantsHub/"}));
+app.get("/*", (req, res) =>
+  res.sendFile("index.html", { root: "dist/PlantsHub/" })
+);
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Server is listening on port ${process.env.PORT || 8080}`);
 });
 
-app.post("/api/create-item/:item_id/:item_name",
-         (req, res) => { api.createItem(pool, req, res); });
+app.post("/api/create-item/:item_id/:item_name", (req, res) => {
+  api.createItem(pool, req, res);
+});
 
-app.post("/api/create-user", (req, res) => { api.createUser(pool, req, res); });
+app.post("/api/create-user", (req, res) => {
+  api.createUser(pool, req, res);
+});
 
-app.post("/api/check-credentials",
-         (req, res) => { api.checkUserCredentials(pool, req, res); });
+app.post("/api/check-credentials", (req, res) => {
+  api.checkUserCredentials(pool, req, res);
+});
 
-app.post("/api/check-session",
-         (req, res) => { api.checkUserSession(pool, req, res); });
+app.post("/api/check-session", (req, res) => {
+  api.checkUserSession(pool, req, res);
+});
