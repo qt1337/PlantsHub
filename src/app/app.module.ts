82224d83875c9aa2194b7cb64128ecbe4a5f1d7e
Qt1337 <d.huh@t-online.de>
2020-11-15 16:42:34 +0100

    :arrow_up: added modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,26 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {AppComponent} from './app.component';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { AppRoutingModule } from './app-routing.module';
+import {SignInComponent} from './sign-in/sign-in.component';
+import {AppRoutingModule} from './app-routing.module';
+import {ReactiveFormsModule} from '@angular/forms';
+import {AuthenticationService} from './authentication.service';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignInComponent
+    SignInComponent,
   ],
   imports: [
     BrowserModule,
     FlexLayoutModule,
     AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ],
   bootstrap: [AppComponent]
 })
 
