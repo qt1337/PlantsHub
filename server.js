80f97e3e6e3cca9a5d48af8fc24ea47e9144a019
Qt1337 <d.huh@t-online.de>
2020-11-16 15:48:08 +0100

    :bug: Fixed cors.

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(bodyParser());
 // app.use(cors());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
