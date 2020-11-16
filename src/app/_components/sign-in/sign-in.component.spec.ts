822fe918f54882457f7de3dca2780785d1f625ab
Qt1337 <d.huh@t-online.de>
2020-11-16 14:54:38 +0100

    :bug: Added modules.

--- src/app/_components/sign-in/sign-in.component.spec.ts
+++ src/app/_components/sign-in/sign-in.component.spec.ts
@@ -1,6 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { SignInComponent } from './sign-in.component';
+import {SignInComponent} from './sign-in.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 describe('SignInComponent', () => {
   let component: SignInComponent;
@@ -8,9 +10,14 @@ describe('SignInComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SignInComponent ]
+      declarations: [SignInComponent],
+      imports: [
+        ReactiveFormsModule,
+        FormsModule,
+        HttpClientModule
+      ],
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
