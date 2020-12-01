3943a57d91815e6a27c187a1d6ec48a5bc5b9dad
David Huh <38101884+qt1337@users.noreply.github.com>
2020-12-01 21:48:58 +0100

    :art: Changed quotation marks.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,15 @@ import {Router, RouterModule, Routes} from '@angular/router';
 import {SignInComponent} from './_components/sign-in/sign-in.component';
 import {SignUpComponent} from './_components/sign-up/sign-up.component';
 import {HomeComponent} from './_components/home/home.component';
-import {AppComponent} from './app.component';
 import {PageNotFoundComponent} from './_components/page-not-found/page-not-found.component';
-import {PlantcardComponent} from "./_components/plantcard/plantcard.component";
+import {PlantcardComponent} from './_components/plantcard/plantcard.component';
 
 const routes: Routes = [
   {path: 'signin', component: SignInComponent},
   {path: 'signup', component: SignUpComponent},
   {path: 'home', component: HomeComponent},
   {path: '', component: HomeComponent},
-  {path: 'plants', component:PlantcardComponent},
+  {path: 'plants', component: PlantcardComponent},
   {path: '404', component: PageNotFoundComponent},
   {path: '**', redirectTo: '/404'},
 
