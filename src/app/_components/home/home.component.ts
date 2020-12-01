8a85fc8a56847dac7b9c3dc735455ce2f88f5e62
Qt1337 <d.huh@t-online.de>
2020-12-01 09:57:34 +0100

    :bug: Redirect on /home if not signed in.

--- src/app/_components/home/home.component.ts
+++ src/app/_components/home/home.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {User} from '../../_models/user';
 import {AuthenticationService} from '../../_services/authentication.service';
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
@@ -10,8 +11,18 @@ import {AuthenticationService} from '../../_services/authentication.service';
 export class HomeComponent implements OnInit {
   user: User;
 
-  constructor(private authenticationService: AuthenticationService) {
-    this.user = this.authenticationService.userValue[0];
+  constructor(
+    private authenticationService: AuthenticationService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    if (this.authenticationService.userValue && this.authenticationService.userValue[0]) {
+      this.user = this.authenticationService.userValue[0];
+    } else {
+      console.log('Please sign in first.');
+      const returnUrl = '/signin';
+      this.router.navigateByUrl(returnUrl);
+    }
   }
 
   ngOnInit(): void {
