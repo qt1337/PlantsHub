00ab0780ba1118952241b8b86b6b3c6c6a9162ed
liza-kl <l.kletsko0202@gmail.com>
2020-12-01 20:19:19 +0100

    :construction: added grid to plantcard

--- src/app/_components/plantcard/plants.ts
+++ src/app/_components/plantcard/plants.ts
@@ -1,18 +1,57 @@
 export const PLANTS = [
   {
     id: 1,
+    family: 'Plantie-Family',
     name: 'Plantie',
     img: 'https://images.pexels.com/photos/2302503/pexels-photo-2302503.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
   },
   {
     id: 2,
+    family: 'Plantie-Family',
     name: 'Plantie',
     img: 'https://images.pexels.com/photos/2302503/pexels-photo-2302503.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
   },
   {
     id: 3,
+    family: 'Plantie-Family',
     name: 'Plantie',
     img: 'https://images.pexels.com/photos/2302503/pexels-photo-2302503.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
   },
+  {
+    id: 4,
+    family: 'Plantie-Family',
+    name: 'Plantie',
+    img: 'https://images.pexels.com/photos/2302503/pexels-photo-2302503.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+  },
+  {
+    id: 5,
+    family: 'Plantie-Family',
+    name: 'Plantie',
+    img: 'https://images.pexels.com/photos/2302503/pexels-photo-2302503.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+  },
+  {
+    id: 6,
+    family: 'Plantie-Family',
+    name: 'Plantie',
+    img: 'https://images.pexels.com/photos/2302503/pexels-photo-2302503.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+  },
+  {
+    id: 7,
+    family: 'Plantie-Family',
+    name: 'Plantie',
+    img: 'https://images.pexels.com/photos/2302503/pexels-photo-2302503.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+  },
+  {
+    id: 8,
+    family: 'Plantie-Family',
+    name: 'Plantie',
+    img: 'https://images.pexels.com/photos/2302503/pexels-photo-2302503.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+  },
+  {
+    id: 9,
+    family: 'Plantie-Family',
+    name: 'Plantie',
+    img: 'https://images.pexels.com/photos/2302503/pexels-photo-2302503.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+  }
 
 ];
