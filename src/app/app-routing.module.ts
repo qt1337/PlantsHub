e3f90877cbc634c0bd9f5989162c655596accfa9
liza-kl <l.kletsko0202@gmail.com>
2020-12-01 09:00:01 +0100

    :sparkles: added 404 page component + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import {Router, RouterModule, Routes} from '@angular/router';
 import {SignInComponent} from './_components/sign-in/sign-in.component';
 import {SignUpComponent} from './_components/sign-up/sign-up.component';
 import {HomeComponent} from './_components/home/home.component';
+import {PageNotFoundComponent} from "./_components/page-not-found-component/page-not-found-component.component";
+import {AppComponent} from "./app.component";
 
 const routes: Routes = [
   {path: 'signin', component: SignInComponent},
   {path: 'signup', component: SignUpComponent},
   {path: 'home', component: HomeComponent},
+  {path: '', component: HomeComponent},
+  {path: '404', component: PageNotFoundComponent},
+  {path: '**', redirectTo: '/404'}
 ];
 
 @NgModule({
