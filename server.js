af0153381b78fcabe03128675f24516177781152
Restyled.io <commits@restyled.io>
2020-12-02 13:55:40 +0000

    Restyled by clang-format

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const plant_api = require("./Backend/plant_api");
 // const cors = require('cors');
 
 const pool = mariadb.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  connectionLimit: 3,
+  host : process.env.DB_HOST,
+  user : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_DATABASE,
+  connectionLimit : 3,
 });
 
 const app = express();
@@ -22,18 +22,16 @@ app.use(express.static("./dist/PlantsHub"));
 app.use(cookieParser());
 app.use(bodyParser());
 // app.use(cors());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin",
+             "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers",
+             "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/PlantsHub/" })
-);
+app.get("/*",
+        (req, res) => res.sendFile("index.html", {root : "dist/PlantsHub/"}));
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Server is listening on port ${process.env.PORT || 8080}`);
@@ -42,29 +40,23 @@ app.listen(process.env.PORT || 8080, () => {
 /**
  * USER API
  */
-app.post("/api/create-user", (req, res) => {
-  user_api.createUser(pool, req, res);
-});
+app.post("/api/create-user",
+         (req, res) => { user_api.createUser(pool, req, res); });
 
-app.post("/api/check-credentials", (req, res) => {
-  user_api.checkUserCredentials(pool, req, res);
-});
+app.post("/api/check-credentials",
+         (req, res) => { user_api.checkUserCredentials(pool, req, res); });
 
-app.post("/api/check-session", (req, res) => {
-  user_api.checkUserSession(pool, req, res);
-});
+app.post("/api/check-session",
+         (req, res) => { user_api.checkUserSession(pool, req, res); });
 
 /**
  * PLANT API
  */
-app.post("/api/create-plant", (req, res) => {
-  plant_api.createPlant(pool, req, res);
-});
+app.post("/api/create-plant",
+         (req, res) => { plant_api.createPlant(pool, req, res); });
 
-app.post("/api/update-plant", (req, res) => {
-  plant_api.updatePlant(pool, req, res);
-});
+app.post("/api/update-plant",
+         (req, res) => { plant_api.updatePlant(pool, req, res); });
 
-app.post("/api/get-plants", (req, res) => {
-  plant_api.getPlants(pool, req, res);
-});
+app.post("/api/get-plants",
+         (req, res) => { plant_api.getPlants(pool, req, res); });
