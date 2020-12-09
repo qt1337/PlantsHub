7fdac2b043aeab26c06cb538ca67b922ec621e61
liza-kl <l.kletsko0202@gmail.com>
2020-12-09 10:01:59 +0100

    :package: added material module to fix the ui problems

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,14 @@ import {FlexLayoutModule} from '@angular/flex-layout';
 import {AppComponent} from './app.component';
 import {SignInComponent} from './_components/sign-in/sign-in.component';
 import {AppRoutingModule} from './app-routing.module';
+import {DemoMaterialModule} from "./material.module";
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {SignUpComponent} from './_components/sign-up/sign-up.component';
 import {HomeComponent} from './_components/home/home.component';
 import {PageNotFoundComponent} from './_components/page-not-found/page-not-found.component';
 import { PlantDialogueComponent } from './_components/plant-dialogue/plant-dialogue.component';
-import { MatDatepickerModule } from "@angular/material/datepicker";
-import {DateAdapter, MatNativeDateModule} from "@angular/material/core";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatFormFieldControl, MatFormFieldModule} from "@angular/material/form-field";
-import {MatInputModule} from "@angular/material/input";
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,16 +24,13 @@ import {MatInputModule} from "@angular/material/input";
   imports: [
     BrowserModule,
     FlexLayoutModule,
-    MatDatepickerModule,
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatNativeDateModule,
-    MatInputModule,
+    DemoMaterialModule
   ],
   providers: [
     HttpClientModule,
