ca961af68ea62e44e93ee583062a4793984faff4
liza-kl <l.kletsko0202@gmail.com>
2020-12-09 08:55:51 +0100

    :construction: added example form to see if it works

--- src/app/_components/plant-dialogue/plant-dialogue.component.ts
+++ src/app/_components/plant-dialogue/plant-dialogue.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
 
 @Component({
   selector: 'app-plant-dialogue',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlantDialogueComponent implements OnInit {
 
-  constructor() { }
+
+  constructor() {
+  }
+
 
   ngOnInit(): void {
   }
