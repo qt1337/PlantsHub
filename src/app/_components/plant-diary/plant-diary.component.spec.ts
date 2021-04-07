dcc5504c05628cf53ce88e26b437ea1b628bc497
liza-kl <l.kletsko0202@gmail.com>
2021-04-07 22:34:31 +0200

    added test bed config to make jasmine happy

--- src/app/_components/plant-diary/plant-diary.component.spec.ts
+++ src/app/_components/plant-diary/plant-diary.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PlantDiaryComponent } from './plant-diary.component';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 
 describe('PlantDiaryComponent', () => {
   let component: PlantDiaryComponent;
@@ -8,8 +9,13 @@ describe('PlantDiaryComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: {} }
+      ],
       declarations: [ PlantDiaryComponent ]
     })
+
     .compileComponents();
   });
 
