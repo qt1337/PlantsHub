952a08a52454a3c06319aa737e18ba2dc54f6fad
liza-kl <l.kletsko0202@gmail.com>
2021-04-23 22:24:03 +0200

    Displaying Username in the Inputfield

--- src/app/account-component/account-component.component.ts
+++ src/app/account-component/account-component.component.ts
@@ -1,19 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {User} from "../_models/user";
+import {UserService} from "../_services/user.service";
 
 @Component({
   selector: 'app-account-component',
   templateUrl: './account-component.component.html',
   styleUrls: ['./account-component.component.scss']
 })
-export class AccountComponent  {
+export class AccountComponent implements OnInit {
   user: User;
   isClicked: boolean;
 
 
-  toggleDisabledAttributInput() : void {
-    const inputField : HTMLElement | unknown = document.getElementsByClassName('account-form-username-input');
+  constructor(private userService : UserService) {
+
+  }
+  toggleDisabledAttributInput(): void {
+    const inputField: HTMLElement | unknown = document.getElementsByClassName('account-form-username-input');
     inputField[0].toggleAttribute("disabled");
     this.isClicked = !this.isClicked;
+  }
+
+  ngOnInit(): void {
+    this.user = this.userService.getUser();
   }
 }
