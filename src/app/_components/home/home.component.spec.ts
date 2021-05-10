73dcb83ca9c3e7275d64a4fcf1049b1e6c0d6748
liza-kl <l.kletsko0202@gmail.com>
2021-05-10 19:54:54 +0200

    :white_check_mark: Test for PlantFavouriteValue

--- src/app/_components/home/home.component.spec.ts
+++ src/app/_components/home/home.component.spec.ts
@@ -1,4 +1,4 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {HomeComponent} from './home.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
@@ -6,11 +6,13 @@ import {HttpClientModule} from '@angular/common/http';
 import {RouterModule} from '@angular/router';
 import {DemoMaterialModule} from '../../material.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {By} from "@angular/platform-browser";
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
+  // Setup
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HomeComponent],
@@ -28,11 +30,12 @@ describe('HomeComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
+    component = fixture.componentInstance;     // test helper
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
--- src/app/_components/plantcard/plantcard.component.spec.ts
+++ src/app/_components/plantcard/plantcard.component.spec.ts
@@ -1,4 +1,4 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, inject, TestBed} from '@angular/core/testing';
 
 import {PlantcardComponent} from './plantcard.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
@@ -8,13 +8,48 @@ import {RouterModule} from '@angular/router';
 import {Ng2SearchPipeModule} from 'ng2-search-filter';
 import {DemoMaterialModule} from '../../material.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AbstractMockObservableService} from "../../_services/mocks/mock.service";
+import {Plant} from "../../_models/plant";
+import {PlantService} from "../../_services/plant.service";
+import {By} from "@angular/platform-browser";
+import any = jasmine.any;
+
+class MockService extends AbstractMockObservableService {
+  updatePlant(plant : Plant) {
+    this.content = [plant];
+    return this;
+  }
+
+  getPlants() {
+    let mockPlant = {
+      plantId: 1,
+      plantName: 'Olga',
+      wateringInterval: 2,
+      fertilizingInterval: 2,
+      plantBirthday: '2020-05-12',
+      plantDeathday: '2020-06-12',
+      family: 'Olgawitsch',
+      type: 'Russian',
+      species: 'Russian',
+      image: 'beautifulimage.png',
+      lux: '200',
+      favourite: false,
+      active: true
+    }
+    this.content = [mockPlant];
+  }
+}
 
 describe('PlantcardComponent', () => {
   let component: PlantcardComponent;
   let fixture: ComponentFixture<PlantcardComponent>;
+  let mockService;
+  let element;
 
   beforeEach(async () => {
+    mockService = new MockService();
     await TestBed.configureTestingModule({
+      providers : [{provide: PlantService, useValue: mockService}],
       declarations: [PlantcardComponent],
       imports: [
         ReactiveFormsModule,
@@ -33,10 +68,37 @@ describe('PlantcardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PlantcardComponent);
     component = fixture.componentInstance;
+    element = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should update the favourite value', () => {
+
+    component.plants = [
+      {
+        plantId: 1,
+        plantName: 'Olga',
+        wateringInterval: 2,
+        fertilizingInterval: 2,
+        plantBirthday: '2020-05-12',
+        plantDeathday: '2020-06-12',
+        family: 'Olgawitsch',
+        type: 'Russian',
+        species: 'Russian',
+        image: 'beautifulimage.png',
+        lux: '200',
+        favourite: false,
+        active: true
+      }
+    ]
+    component.ngOnInit();
+    fixture.detectChanges();
+    const favouriteButtonElement = fixture.debugElement.query(By.css('[name=heart]'));
+    favouriteButtonElement.nativeElement.click();
+    expect(component.plants[0].favourite).toBe(true);
   });
 });
