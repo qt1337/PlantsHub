ab867087c0f7e16163276555eea3e42cde8de0f4
David <D.Huh@t-online.de>
2021-05-20 11:41:08 +0200

    :construction: WIP

--- src/app/_components/account-component/account-component.component.ts
+++ src/app/_components/account-component/account-component.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit, Output} from '@angular/core';
-import {User} from "../../_models/user";
-import {UserService} from "../../_services/user.service";
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import {User} from '../../_models/user';
+import {UserService} from '../../_services/user.service';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {AuthenticationService} from '../../_services/authentication.service';
 
 @Component({
   selector: 'app-account-component',
@@ -13,20 +14,24 @@ export class AccountComponent implements OnInit {
   nameChangeLog: string[] = [];
   user: User;
   inputFields: string[];
-  inputFieldTitles: string[] = ['Username','Email','Forename','Surname','Birthday']
+  inputFieldTitles: string[] = ['Username', 'Email', 'Forename', 'Surname', 'Birthday'];
 
-  SERVER_URL = "http://localhost:8080/profile";
+  SERVER_URL = 'http://localhost:8080/profile';
   isClicked: boolean;
   updateUserInformationForm: FormGroup;
 
-  constructor(private userService : UserService) {
+  constructor(
+    private userService: UserService,
+    private authenticationService: AuthenticationService,
+  ) {
 
   }
-  toggleDisabledAttributInput(): void {
-    let inputFields: HTMLElement | any = document.getElementsByClassName('account-form-input');
-    let inputFieldsArray = [...inputFields];
 
-    inputFieldsArray.map(inputField => inputField.toggleAttribute("disabled"));
+  toggleDisabledAttributInput(): void {
+    const inputFields: HTMLElement | any = document.getElementsByClassName('account-form-input');
+    const inputFieldsArray = [...inputFields];
+
+    inputFieldsArray.map(inputField => inputField.toggleAttribute('disabled'));
     this.isClicked = !this.isClicked;
   }
 
@@ -34,14 +39,25 @@ export class AccountComponent implements OnInit {
   ngOnInit(): void {
     this.user = this.userService.getUser();
     this.inputFields = this.userService.getUserValues();
-    this.updateUserInformation();
   }
 
   updateUserInformation() {
     //tbd
   }
 
-  onSubmit() {
-    // tbd
+
+  // TODO: Lisa pls implement these two functions [update, delete]
+  deleteUser(): void {
+    this.authenticationService.deleteUser();
+  }
+
+  updateUser(): void {
+    this.authenticationService.updateUser(
+      this.inputFields[0],
+      this.inputFields[1],
+      this.inputFields[2],
+      this.inputFields[3],
+      this.inputFields[4]
+    );
   }
 }
